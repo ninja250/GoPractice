fccb89db72af19f86974e4623201b55f615b4f53
ninja250 <naitomea.mky@gmail.com>
2019-04-22 14:34:04 +0900

    Update Scan01.go

--- Scan01.go
+++ Scan01.go
@@ -1,12 +1,35 @@
 package main
+// import (
+//   "fmt"
+//   // "os"
+//   // "bufio"
+// )
+// func main(){
+//   // stdin := bufio.NewScanner(os.Stdin)
+//   // stdin.Scan()
+//   // text := stdin.Text()
+//   // fmt.Printf(text\n)
+//   fmt.Println("Go言語はじめました！")
+// }
 import (
-  "fmt"
-  "os"
-  "bufio"
+    "fmt"
+    "os"
+    "bufio"
+    "strings"
 )
-main(){
-  stdin := bufio.NewScanner(os.Stdin)
-  stdin.Scan()
-  text := stdin.Text()
-  fmt.Printf(text\n)
+
+// 文字列を1行入力
+func StrStdin() (stringInput string) {
+    scanner := bufio.NewScanner(os.Stdin)
+
+    scanner.Scan()
+    stringInput = scanner.Text()
+
+    stringInput = strings.TrimSpace(stringInput)
+    return
+}
+
+func main() {
+    p := StrStdin()
+    fmt.Println(p)
 }
